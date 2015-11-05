e625d746bcd475e4e9018e57c3df6548e8f947dd
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2015-11-05 12:52:41 +0100

    Try app import

--- index.js
+++ index.js
@@ -2,13 +2,19 @@
 'use strict';
 
 var path = require('path');
+var Funnel = require('broccoli-funnel');
 
 module.exports = {
   name: 'observable',
 
-  treeForAddon: function() {
-    var srcPath = path.join(this.project.addonPackages['ember-observable-shim'].path, 'node_modules', 'zen-observable', 'src');
-    var tree = this.treeGenerator(srcPath);
-    return this._super.treeForAddon.call(this, tree);
+  included: function(app) {
+    var nodeModulesPath = path.join(this.project.addonPackages['ember-observable-shim'].path, 'node_modules');
+    app.import(path.join(nodeModulesPath, 'zen-observable', 'zen-observable.js'), { prepend: true });
   }
+
+  // treeForAddon: function(tree) {
+  //   var srcPath = path.join(this.project.addonPackages['ember-observable-shim'].path, 'node_modules', 'zen-observable', 'src');
+  //   var srcTree = this.treeGenerator(srcPath);
+  //   return this._super.treeForAddon.call(this, srcTree);
+  // }
 };
